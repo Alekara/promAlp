13b00f746a131a0a2632ff95aa22c6e6111eeade
Alekara <43556614+Alekara@users.noreply.github.com>
2018-10-26 22:25:03 +0300

    Доделала слайдер, унифицировала часть кода, исправила заголовок на главном экране

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ var taskPlace = document.querySelector(".objects__task");
 namePlace.textContent = "Национальный аэропорт Минск-2";
 taskPlace.textContent = "очистка терминалов от снега и наледи";
 
-document.querySelector(".objects__btn--down").addEventListener("click", function () {
+document.querySelector(".slider__btn--down").addEventListener("click", function () {
   fromTop = fromTop - 400;
-  if (fromTop < -1200) {
+  if (fromTop < -2400) {
     fromTop = 0;
   }
   checkTop();
 })
 
-document.querySelector(".objects__btn--up").addEventListener("click", function () {
+document.querySelector(".slider__btn--up").addEventListener("click", function () {
   if (fromTop == 0) {
-    fromTop = fromTop - 1200;
+    fromTop = fromTop - 2400;
   } else {
     fromTop = fromTop + 400;
   }
@@ -34,9 +34,17 @@ function checkTop () {
   } else if (fromTop == -800) {
     namePlace.textContent = "Стадион \"Динамо\"";
     taskPlace.textContent = "монтаж металлоконструкций, ремонт балок, панелей, кровли, ходовых мостиков";
-
   } else if (fromTop == -1200) {
     namePlace.textContent = "Кинотеатр \"Мир\"";
     taskPlace.textContent = "демонтаж вывески";
+  } else if (fromTop == -1600) {
+    namePlace.textContent = "Музей ВОВ";
+    taskPlace.textContent = "монтаж осветительных приборов";
+  } else if (fromTop == -2000) {
+    namePlace.textContent = "Гостиница \"Ренессанс\"";
+    taskPlace.textContent = "монтаж вентфасада, различные ремонтные работы";
+  } else if (fromTop == -2400) {
+    namePlace.textContent = "Производственный логистический комплекс (Сморгонь)";
+    taskPlace.textContent = "монтаж сэндвич-панелей";
   }
 };
