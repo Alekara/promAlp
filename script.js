d9c6b2000e15274e7dd500b9d8c0bb2cb34144e6
Alekara <43556614+Alekara@users.noreply.github.com>
2018-11-10 16:17:02 +0300

    сделала мини-слайдер в секции "О нас", привела к единому виду почти все секции типа about

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var fromTop = 0;
 var namePlace = document.querySelector(".objects__name");
 var taskPlace = document.querySelector(".objects__task");
 
+var leftArrow = document.querySelector(".why-us__left-arrow");
+var rightArrow = document.querySelector(".why-us__right-arrow");
+
+var item1 = document.querySelector(".why-us__item--1");
+var item2 = document.querySelector(".why-us__item--2");
+var item3 = document.querySelector(".why-us__item--3");
+var item4 = document.querySelector(".why-us__item--4");
+var item5 = document.querySelector(".why-us__item--5");
+var item6 = document.querySelector(".why-us__item--6");
+var item7 = document.querySelector(".why-us__item--7");
+
+var items = [item1, item2, item3, item4, item5, item6, item7];
+var current = 0;
+
 namePlace.textContent = "Национальный аэропорт Минск-2";
 taskPlace.textContent = "очистка терминалов от снега и наледи";
 
@@ -11,7 +25,7 @@ document.querySelector(".slider__btn--down").addEventListener("click", function 
     fromTop = 0;
   }
   checkTop();
-})
+});
 
 document.querySelector(".slider__btn--up").addEventListener("click", function () {
   if (fromTop == 0) {
@@ -20,7 +34,7 @@ document.querySelector(".slider__btn--up").addEventListener("click", function ()
     fromTop = fromTop + 400;
   }
   checkTop();
-})
+});
 
 function checkTop () {
   var slider = document.querySelector(".slider__stripe");
@@ -48,3 +62,30 @@ function checkTop () {
     taskPlace.textContent = "монтаж сэндвич-панелей";
   }
 };
+
+rightArrow.addEventListener("click", clickRightArrow);
+leftArrow.addEventListener("click", clickLeftArrow);
+
+function clickRightArrow() {
+  items[current].style.display = "none";
+  if (current == items.length - 1) {
+    current = 0;
+  } else {
+    ++current;
+  }
+  items[current].style.display = "block";
+  return current;
+};
+
+function clickLeftArrow() {
+  items[current].style.display = "none";
+  if (current == 0) {
+    current = items.length - 1;
+  } else {
+    --current;
+  }
+  items[current].style.display = "block";
+  return current;
+};
+
+
