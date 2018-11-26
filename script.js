9a734d5ffb7dfab3638420069ab7c956ea5d222f
Alex Karavaeva <carawaeva@gmail.com>
2018-11-26 23:34:09 +0300

    полностью переверстала сайт и добавила версии для меньшего разрешения экрана

--- script.js
+++ script.js
@@ -1,91 +1,110 @@
-var fromTop = 0;
-var namePlace = document.querySelector(".objects__name");
-var taskPlace = document.querySelector(".objects__task");
+//СКРИПТЫ ДЛЯ СЛАЙДЕРОВ
 
 var leftArrow = document.querySelector(".why-us__left-arrow");
 var rightArrow = document.querySelector(".why-us__right-arrow");
+var benefits = document.querySelectorAll(".why-us__item");
 
-var item1 = document.querySelector(".why-us__item--1");
-var item2 = document.querySelector(".why-us__item--2");
-var item3 = document.querySelector(".why-us__item--3");
-var item4 = document.querySelector(".why-us__item--4");
-var item5 = document.querySelector(".why-us__item--5");
-var item6 = document.querySelector(".why-us__item--6");
-var item7 = document.querySelector(".why-us__item--7");
+var upArrow = document.querySelector(".slider__btn--up");
+var downArrow = document.querySelector(".slider__btn--down");
+var namePlace = document.querySelector(".objects__name");
+var taskPlace = document.querySelector(".slider__task");
+var slides = document.querySelectorAll(".slider__photo");
 
-var items = [item1, item2, item3, item4, item5, item6, item7];
-var current = 0;
+var currentSlide = 0;
+var currentBenefit = 0;
+
+//СЛАЙДЕР ПРЕИМУЩЕСТВ
+
+rightArrow.addEventListener("click", clickRightArrow);                         
+leftArrow.addEventListener("click", clickLeftArrow);
+
+function clickRightArrow() {
+  benefits[currentBenefit].style.display = "none";
+  if (currentBenefit == benefits.length - 1) {
+    currentBenefit = 0;
+  } else {
+    ++currentBenefit;
+  }
+  benefits[currentBenefit].style.display = "block";  
+};
+
+function clickLeftArrow() {
+  benefits[currentBenefit].style.display = "none";
+  if (currentBenefit == 0) {
+    currentBenefit = benefits.length - 1;
+  } else {
+    --currentBenefit;
+  }
+  benefits[currentBenefit].style.display = "block";
+};
+
+
+//СЛАЙДЕР ОБЪЕКТОВ
+
+upArrow.addEventListener("click", clickUpArrow);                         
+downArrow.addEventListener("click", clickDownArrow);
+
+function clickDownArrow() {
+  slides[currentSlide].style.display = "none";
+  if (currentSlide == slides.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  slides[currentSlide].style.display = "block";
+  checkSlide();  
+};
+
+function clickUpArrow() {
+  slides[currentSlide].style.display = "none";
+  if (currentSlide == 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide--;
+  }
+  slides[currentSlide].style.display = "block";
+  checkSlide();
+};
 
 namePlace.textContent = "Национальный аэропорт Минск-2";
 taskPlace.textContent = "очистка терминалов от снега и наледи";
 
-document.querySelector(".slider__btn--down").addEventListener("click", function () {
-  fromTop = fromTop - 400;
-  if (fromTop < -2400) {
-    fromTop = 0;
-  }
-  checkTop();
-});
 
-document.querySelector(".slider__btn--up").addEventListener("click", function () {
-  if (fromTop == 0) {
-    fromTop = fromTop - 2400;
-  } else {
-    fromTop = fromTop + 400;
-  }
-  checkTop();
-});
-
-function checkTop () {
-  var slider = document.querySelector(".slider__stripe");
-  slider.style.top = fromTop + "px";
-  if (fromTop == 0) {
+function checkSlide() {
+  if (currentSlide == 0) {
     namePlace.textContent = "Национальный аэропорт Минск-2";
     taskPlace.textContent = "очистка терминалов от снега и наледи";
-  } else if (fromTop == -400) {
+  } else if (currentSlide == 1) {
     namePlace.textContent = "Гостиница \"Виктория\"";
     taskPlace.textContent = "ремонт фасада, устранение течи, задувания и промерзания, замена лопнувших витражей";
-  } else if (fromTop == -800) {
+  } else if (currentSlide == 2) {
     namePlace.textContent = "Стадион \"Динамо\"";
     taskPlace.textContent = "монтаж металлоконструкций, ремонт балок, панелей, кровли, ходовых мостиков";
-  } else if (fromTop == -1200) {
+  } else if (currentSlide == 3) {
     namePlace.textContent = "Кинотеатр \"Мир\"";
     taskPlace.textContent = "демонтаж вывески";
-  } else if (fromTop == -1600) {
+  } else if (slides[currentSlide] == slides[4]) {
     namePlace.textContent = "Музей ВОВ";
     taskPlace.textContent = "монтаж осветительных приборов";
-  } else if (fromTop == -2000) {
+  } else if (slides[currentSlide] == slides[5]) {
     namePlace.textContent = "Гостиница \"Ренессанс\"";
     taskPlace.textContent = "монтаж вентфасада, различные ремонтные работы";
-  } else if (fromTop == -2400) {
+  } else if (slides[currentSlide] == slides[6]) {
     namePlace.textContent = "Производственный логистический комплекс (Сморгонь)";
     taskPlace.textContent = "монтаж сэндвич-панелей";
   }
-};
+}
 
-rightArrow.addEventListener("click", clickRightArrow);
-leftArrow.addEventListener("click", clickLeftArrow);
+//СКРИПТ ДЛЯ КНОПОК "ПОКАЗАТЬ МЕНЮ/СКРЫТЬ МЕНЮ"
 
-function clickRightArrow() {
-  items[current].style.display = "none";
-  if (current == items.length - 1) {
-    current = 0;
-  } else {
-    ++current;
-  }
-  items[current].style.display = "block";
-  return current;
-};
+var menuToggle = document.querySelector(".main-nav__toggle");
 
-function clickLeftArrow() {
-  items[current].style.display = "none";
-  if (current == 0) {
-    current = items.length - 1;
-  } else {
-    --current;
-  }
-  items[current].style.display = "block";
-  return current;
-};
+menuToggle.addEventListener("click", function () {
+  document.querySelector(".main-nav__list").classList.toggle("main-nav__list--hidden");
+  document.querySelector(".main-nav").classList.toggle("main-nav--hidden");
+  menuToggle.classList.toggle("main-nav__toggle--menu-opened");
+  document.querySelector(".main-nav__icon").classList.toggle("main-nav__icon--cross");
+});
+
 
 
